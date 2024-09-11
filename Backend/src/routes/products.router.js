f40baa87137c817ba3a58dbc2fe4375757e0ae09
Roberto Pach <btofpach@gmail.com>
2024-09-10 22:58:03 -0300

    cambio en products.router

--- Backend/src/routes/products.router.js
+++ Backend/src/routes/products.router.js
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 const router = Router();
 
@@ -6,15 +6,83 @@ const router = Router();
 let products = [];
 
 //Obtener todos los productos
-router.get('/', (req, res) => {
-    res.json(products);
+router.get("/", (req, res) => {
+  res.json(products);
 });
 
-//Crear nuevo producto
-router.post('/', (req, res) => {
-    const newProducts = req.body;
-    products.push(newProducts);
-    res.status(201).json(newProducts);
-} )
+//Crea un producto
+router.post("/", (req, res) => {
+  const { title, description, code, price, stock, category, thumbnails } =
+    req.body;
+  if (!title || !description || !code || !price || !stock || !category) {
+    return res
+      .status(400)
+      .send("Todos los parametos son requeridos exepto thumbnails ");
+  }
+  const newProduct = {
+    id: products.length + 1,
+    title,
+    description,
+    code,
+    price,
+    status: true,
+    stock,
+    category,
+    thumbnails: thumbnails || [],
+  };
+  products.push(newProduct);
+  res.status(201).json(newProduct);
+});
+
+// Obtiene un producto por id
+
+router.get("/:pid", (req, res) => {
+  const productId = parseInt(req.params.pid);
+  const product = products.find((p) => p.id === productId);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).send("Producto no encontrado");
+  }
+});
+
+// Modifica un producto por id
+router.put("/:pid", (req, res) => {
+  const productId = parseInt(req.params.pid);
+  const productIndex = products.findIndex((p) => p.id === productId);
+  if (productIndex === -1) {
+    return res.status(404).send("Producto no encontrado");
+  }
+  const {
+    title,
+    description,
+    code,
+    price,
+    status,
+    stock,
+    category,
+    thumbnails,
+  } = req.body;
+  const updatedProduct = {
+    ...products[productIndex],
+    title,
+    description,
+    code,
+    price,
+    status,
+    stock,
+    category,
+    thumbnails,
+  };
+  products[productIndex] = updatedProduct;
+  res.json(updatedProduct);
+});
+
+// Borra un producto por id
+router.delete("/:pid", (req, res) => {
+  const productId = parseInt(req.params.pid);
+  products = products.filter((p) => p.id !== productId);
+  res.status(204).send();
+});
 
 export default router;
